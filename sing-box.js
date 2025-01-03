adaf132c92f6d1ff91354b67479e751a171d46f6
kaining202304-ai <kaining202304@gmail.com>
2025-01-03 15:33:38 +0800

    Update sing-box.js

--- sing-box.js
+++ sing-box.js
@@ -6,10 +6,15 @@ let proxies = await produceArtifact({
   platform: 'sing-box',
   produceType: 'internal',
 })
+
+// 定义需要过滤的 tag 列表
+const excludedTags = ["hk", "hk-auto", "tw", "tw-auto", "jp", "jp-auto", "sg", "sg-auto", "us", "us-auto", "all", "all-auto"];
+
 config.outbounds.push(...proxies)
 config.outbounds.map(i => {
   if (['select', 'auto'].includes(i.tag)) {
-    i.outbounds.push(...getTags(proxies))
+    // 过滤掉 excludedTags 中的 tag
+    i.outbounds.push(...getTags(proxies).filter(tag => !excludedTags.includes(tag)));
   }
 })
 $content = JSON.stringify(config, null, 2)
