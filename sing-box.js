16dd3a9a6a8631f50597acb01ee5a0998fa1cccd
kaining202304-ai <kaining202304@gmail.com>
2025-01-04 11:52:49 +0800

    Update sing-box.js

--- sing-box.js
+++ sing-box.js
@@ -6,16 +6,28 @@ let proxies = await produceArtifact({
   platform: 'sing-box',
   produceType: 'internal',
 })
+
 // 定义需要过滤的 tag 列表
-const excludedTags = ["hk", "hk-auto", "tw", "tw-auto", "jp", "jp-auto", "sg", "sg-auto", "us", "us-auto", "all", "all-auto"];
+const excludedTags = ["hk", "hk-auto", "tw", "tw-auto", "jp", "jp-auto", "sg", "sg-auto", "us", "us-auto", "剩余流量","过期时间"];
+
+// 在添加到 config.outbounds 之前过滤 proxies
+proxies = proxies.filter(proxy => {
+  return !excludedTags.includes(proxy.tag) &&
+         !proxy.tag.includes("剩余流量") &&
+         !proxy.tag.includes("过期时间");
+});
+
 config.outbounds.push(...proxies)
+
 config.outbounds.map(i => {
   if (['select', 'auto'].includes(i.tag)) {
-    // 过滤掉 excludedTags 中的 tag 以及包含 "剩余流量" 或 "过期时间" 的 tag
+    // 此时 getTags 应该不会再返回包含 "剩余流量" 或 "过期时间" 的标签
     i.outbounds.push(...getTags(proxies).filter(tag => !excludedTags.includes(tag)));
   }
 })
+
 $content = JSON.stringify(config, null, 2)
+
 function getTags(proxies, regex) {
   return (regex ? proxies.filter(p => regex.test(p.tag)) : proxies)
     .map(p => p.tag)
